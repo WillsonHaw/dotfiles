308c067110e71f45371f9119c5df7ef473361b52
WillsonHaw <willsonhaw@gmail.com>
2024-06-03 11:04:53 -0700

    Fixes for eww

--- nixos2/hosts/laptop/default.nix
+++ nixos2/hosts/laptop/default.nix
@@ -24,9 +24,10 @@
     device.is-laptop = true;
 
     services.power.enable = true;
-    #    desktops.gnome.enable = true;
-    desktops.hyprland.enable = true;
-    desktops.hyprland.card = "/dev/dri/by-path/pci-0000:00:02.0-card";
+    # desktops.gnome.enable = true;
+    # desktops.hyprland.enable = true;
+    desktops.hyprland_new.enable = true;
+    desktops.hyprland_new.card = "/dev/dri/by-path/pci-0000:00:02.0-card";
 
     browsers.vivaldi.enable = true;
     browsers.floorp.enable = true;
