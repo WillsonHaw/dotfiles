4797357951e9ee43013035d88cb6af386bf6f3ac
WillsonHaw <willsonhaw@gmail.com>
2024-06-04 16:26:54 -0700

    Fix workspaces in eww

--- nixos2/modules/services/keyring/default.nix
+++ nixos2/modules/services/keyring/default.nix
@@ -9,5 +9,11 @@
   # programs.seahorse.enable = true;
   services.gnome.gnome-keyring.enable = true;
 
+  security.pam.services = {
+    #   greetd.enableKwallet = true;
+    greetd.enableGnomeKeyring = true;
+    #   swaylock = { };
+  };
+
   # environment.systemPackages = with pkgs; [ gnome.seahorse ];
 }
