4e356f2f10343f92432639a16757d7b0c75a9bfb
WillsonHaw <willsonhaw@gmail.com>
2026-05-31 15:07:34 -0700

    Add laptop to authorized keys

--- nixos/users/slumpy.nix
+++ nixos/users/slumpy.nix
@@ -28,6 +28,7 @@ in
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILXtwwCwAQNOW6YZuMpUoOzEGmDKK5W4WQpKd21jKtvw willsonhaw@gmail.com"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJhc6E9VyPuA3SMJuU3aRJkVw/xfRlT9hECU40py7vgk slumpy@slumpy-desktop"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE1WXDv8fc5OwenVI+910X6xJUBcl/HLqIDVqL44Ghsl willsonhaw@gmail.com" # Gaming Desktop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYSW/qZVvdPLji9Qj1hNiAFXMGJzXO/jxdSMCjaA/TA slumpy@nixos" # MSI Laptop
     ];
   };
 
